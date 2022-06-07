08e7b1ecfcd980dd05403a54ccc1edc146ab29b0
NourEsprit <nour.boumnijel1@esprit.tn>
2022-06-07 09:05:36 +0100

    modif 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
+// modif 2
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
